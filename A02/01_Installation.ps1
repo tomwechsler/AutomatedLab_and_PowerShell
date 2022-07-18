c3c775a7caf069ac6d7688655859eec539bb2bd6
Tom Wechsler <tom.wechsler@outlook.com>
2022-07-18 15:21:17 +0200

    add a new line

--- A02/01_Installation.ps1
+++ A02/01_Installation.ps1
@@ -1,5 +1,8 @@
-#Hyper-V Installation
+#Hyper-V Installation on a Server
 Install-WindowsFeature -Name Hyper-V -ComputerName <computer_name> -IncludeManagementTools -Restart
+
+#Hyper-V Installation on a Windows 10/11
+Enable-WindowsOptionalFeature -Online -FeatureName Microsoft-Hyper-V -All
 
 #MSI-Download:
 https://automatedlab.org/en/latest/Wiki/Basic/install/
