4ce354fbcc252bbb2b3884089e967706d4a21af6
Tom Wechsler <tom.wechsler@outlook.com>
2022-07-21 16:55:51 +0200

    new files

---
+++ A03/03_Single_VM_Internet_Connection.ps1
@@ -0,0 +1,17 @@
+#Show available OS
+Get-LabAvailableOperatingSystem -Path C:\LabSources
+
+#Install Lab and VM
+New-LabDefinition -Name Win10 -DefaultVirtualizationEngine HyperV
+
+#Lab Machine Definition
+Add-LabVirtualNetworkDefinition -Name 'Default Switch' -HyperVProperties @{ SwitchType = 'External'; AdapterName = 'Ethernet 3' }
+Add-LabMachineDefinition -Name Client1 -Memory 1GB -OperatingSystem 'Windows 10 Enterprise Evaluation'
+
+#Install Lab
+Install-Lab
+
+#We didn't specify a password, the default is: Somepass1
+
+#Remove Lab
+Remove-Lab -Path C:\ProgramData\AutomatedLab\Labs\Win10
